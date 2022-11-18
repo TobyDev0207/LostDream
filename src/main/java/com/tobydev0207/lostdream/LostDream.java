0708c819753deb85e570d2acfcf9292e2488a89e
TobyDev0207 <118341149+TobyDev0207@users.noreply.github.com>
2022-11-18 20:42:50 +0700

    Update mod info

---
+++ src/main/java/com/tobydev0207/lostdream/LostDream.java
@@ -0,0 +1,10 @@
+package com.tobydev0207.lostdream;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LostDream implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
